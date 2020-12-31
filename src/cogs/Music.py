d94a0bff1dd0b6eca7f2707debab3dff50f0b57b
Allvaa <alvamailindo@gmail.com>
2020-12-31 21:18:21 +0700

    queue handler

--- src/cogs/Music.py
+++ src/cogs/Music.py
@@ -19,5 +19,34 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
                                               identifier="main",
                                               region=os.environ.get("LAVA_REGION"))
 
+    @wavelink.WavelinkMixin.listener()
+    async def on_track_start(self, node: wavelink.Node, payload: wavelink.TrackStart):
+        music = self.bot.get_music(payload.player.guild_id)
+        music.current = music.queue.pop(0)
+
+        if music.text_channel: await music.text_channel.send(f"Now playing {music.current.title}")
+
+    @wavelink.WavelinkMixin.listener()
+    async def on_track_end(self, node: wavelink.Node, payload: wavelink.TrackEnd):
+        if payload.reason == "REPLACED": return
+
+        music = self.bot.get_music(payload.player.guild_id)
+
+        music.previous = music.current
+        music.current = None
+
+        if music.loop: music.queue.append(music.previous)
+
+        if not music.queue:
+            if music.text_channel: await music.text_channel.send("Queue is empty")
+            return await music.destroy()
+
+        await music.start()
+
+    @wavelink.WavelinkMixin.listener()
+    async def on_track_exception(self, node: wavelink.Node, payload: wavelink.TrackException):
+        music = self.bot.get_music(payload.player.guild_id)
+        if music.text_channel: await music.text_channel.send(f"An error occured: {payload.error}")
+
 def setup(bot: MusicBot):
     bot.add_cog(Music(bot))
