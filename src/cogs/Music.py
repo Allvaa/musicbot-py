6990a101d5e325f466bcd8f27e200cd4c02c4da8
Allvaa <alvamailindo@gmail.com>
2020-12-31 22:09:35 +0700

    add play command

--- src/cogs/Music.py
+++ src/cogs/Music.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from src.structures.client import MusicBot
 import wavelink
 import os
+import re
 
 class Music(commands.Cog, wavelink.WavelinkMixin):
     def __init__(self, bot: MusicBot) -> None:
@@ -18,6 +19,44 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
                                               password=os.environ.get("LAVA_PASS"),
                                               identifier="main",
                                               region=os.environ.get("LAVA_REGION"))
+
+    @commands.command()
+    async def play(self, ctx: commands.Context, *, query: str):
+        if not await self.check_vc(ctx): return
+        tracks = await self.bot.wavelink.get_tracks(query if self.is_valid_url else f"ytsearch: {query}")
+
+        if not tracks:
+            return await ctx.send("Could not find any songs with that query.")
+
+        music = self.bot.get_music(ctx.guild.id)
+        player = music.player()
+        if not player.is_connected:
+            await music.join(ctx.author.voice.channel)
+
+        music.queue.append(tracks[0])
+        if player.is_playing: await ctx.send(f"Added {str(tracks[0])} to the queue.")
+        if not player.is_playing:
+            await music.start()
+        music.set_text_channel(ctx.channel)
+
+    async def check_vc(self, ctx: commands.Context) -> bool:
+        if not ctx.author.voice:
+            await ctx.send("You aren't connected to any voice channels")
+            return False
+        elif ctx.me.voice and ctx.me.voice.channel != ctx.author.voice.channel:
+            await ctx.send(f"You need to be in {ctx.me.voice.channel.mention}")
+            return False
+        else: return True
+
+    async def check_playing(self, ctx: commands.Context) -> bool:
+        music = self.bot.get_music(ctx.guild.id)
+        if not music.player().is_playing:
+            await ctx.send("Currently not playing anything")
+            return False
+        return True
+
+    def is_valid_url(url: str) -> bool:
+        return bool(re.compile(r"^https?:\/\/((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?$", flags=re.I).match(url))
 
     @wavelink.WavelinkMixin.listener()
     async def on_track_start(self, node: wavelink.Node, payload: wavelink.TrackStart):
